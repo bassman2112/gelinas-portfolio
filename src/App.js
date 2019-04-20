f30bf5096cc801b40003f57705fc84fe56333ff3
Alexandre Gelinas <gelinas.j.alexandre@gmail.com>
2019-04-19 22:40:59 -0700

    cleaned up app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+// Routes
 import Home from './routes/home'
-
-
-// Use Testimonials if you want! I don't use it. 
-//import Testimonials from  './components/Testimonials';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          {/* --------------------------------------------------
-          | This is an ultra simple router which allows us to  |
-          | render the 'Home' component no matter which route  |
-          | comes in! Adding more routes from here is very ez. |
-          ----------------------------------------------------*/}
           <Route exact path="/" component={Home} />
           <Route component={Home} />
         </Switch>
