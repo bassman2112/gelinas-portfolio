807b4dcdc3dc985fc97902e5d97a1db064cb29bb
Alex <gelinas.j.alexandre@gmail.com>
2019-04-29 12:17:37 -0700

    Removed Suspense - will add later for svg trace

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import React, { Component, Suspense } from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // Routes
-//import Home from "./routes/home";
-
-const Home = React.lazy(() => import("./routes/home"));
+import Home from "./routes/home";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Suspense
-            fallback={
-              <div>
-                <p className="loadingtext">Loading...</p>
-              </div>
-            }
-          >
-            <Route exact path="/" component={Home} />
-          </Suspense>
-
-          <Suspense
-            fallback={
-              <div>
-                <p className="loadingtext">Loading...</p>
-              </div>
-            }
-          >
-            <Route component={Home} />
-          </Suspense>
+          <Route exact path="/" component={Home} />
+          <Route component={Home} />
         </Switch>
       </BrowserRouter>
     );
